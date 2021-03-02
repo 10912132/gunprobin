33c3ced7da1f5ad2b9fb9e48ee579823a5736831
10912132 <79931202+10912132@users.noreply.github.com>
2021-03-03 01:26:13 +0800

    Add files via upload

---
+++ myArithemtic.py
@@ -0,0 +1,15 @@
+'''myArithmetic'''
+def  myAdd（x，y）：
+    返回 x  +  y
+
+def  mySub（x，y）：
+    返回 x  - y
+
+def  myMultiply（x，y）：
+    返回 x * y
+
+def  myDiv（x，y）：
+    如果 y  ！=  0：
+        返回 x / y
+    其他：
+        返回 False
---
+++ myCalcArea.py
@@ -0,0 +1,12 @@
+'''myCalcArea'''
+PI  =  3.1415
+def  RectArea（w，l）：
+  返回 w  *  l
+def  TriArea（s，h）：
+  返回 s  *  h  /  2
+def  SquareArea（s）：
+  返回 s  *  s ;
+def  CircleArea（r）：
+  返回 PI  *  r  *  r
+
+
